15596b1d514c0a791ffa7a29b101982bc75e42a0
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-11-10 16:01:03 -0500

    adds pokemon and dice tests and classes

--- spec/lib/dice_spec.rb
+++ spec/lib/dice_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe Dice do
-  
+
+  describe ".new" do
+    dice = Dice.new(6)
+    it "has sides" do
+      expect(dice.sides).to eq 6
+    end
+  end
+
+  describe "#roll" do
+    dice = Dice.new(6)
+    it "return a number from one of the sides" do
+      expect((1..6).to_a).to include(dice.roll)
+    end
+  end
+
 end
